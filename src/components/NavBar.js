4244c01f6d4851fa6433c1ef57cfe87a07d72526
John <scruffy403@gmail.com>
2021-08-14 21:14:07 -0700

    add and style navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,66 @@
-import React from "react"
+import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
-    return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
-                        Soto
-                    </NavLink>
-                    <NavLink to="/post">
-                        Blog Posts
-                    </NavLink>
-                    <NavLink to="/project">
-                        Projects
-                    </NavLink>
-                    <NavLink to="/about">
-                        About Me
-                    </NavLink>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header className="bg-blue-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-yellow-500 text-4xl font-bold cursive tracking-widest"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            Soto
+          </NavLink>
+          <NavLink
+            to="/post"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            Blog Posts
+          </NavLink>
+          <NavLink
+            to="/project"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+            activeClassName="text-blue-100 bg-blue-700"
+          >
+            About Me
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-6 px-3 mx-6">
+          <SocialIcon
+            url="https://twitter.com/JohnDavidSoto1"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.instagram.com/scruffy403/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.facebook.com/johndavid.soto.3"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
 }
