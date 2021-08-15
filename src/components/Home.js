0a703327347952df10e1ca0393187e69856b8412
John <scruffy403@gmail.com>
2021-08-14 21:50:13 -0700

    completed and styled home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react"
+import image from "../guitar.jpg";
 
-export default function Home() {
-    return <h1>Home Page!</h1>
+export default function About() {
+    return (
+        <main>
+            <img src={image} alt="Closeup of guitar soundhole and strings" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-blue-400 font-bold cursive leading-none lg:leading-snug home-name">Hello! I'm JD</h1>
+            </section>
+        </main>
+    )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,25 +13,25 @@ export default function NavBar() {
             className="inflex-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-yellow-500 text-4xl font-bold cursive tracking-widest"
             activeClassName="text-blue-100 bg-blue-700"
           >
-            Soto
+            JD
           </NavLink>
           <NavLink
             to="/post"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-100 hover:text-yellow-500"
             activeClassName="text-blue-100 bg-blue-700"
           >
             Blog Posts
           </NavLink>
           <NavLink
             to="/project"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-100 hover:text-yellow-500"
             activeClassName="text-blue-100 bg-blue-700"
           >
             Projects
           </NavLink>
           <NavLink
             to="/about"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-yellow-500"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-blue-100 hover:text-yellow-500"
             activeClassName="text-blue-100 bg-blue-700"
           >
             About Me
