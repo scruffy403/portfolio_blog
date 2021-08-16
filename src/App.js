d44b033445912f46197fab6df52bbbc03d1873b8
John <scruffy403@gmail.com>
2021-08-16 10:41:34 -0700

    changed SinglePost Route to post:slug to get it working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
 import About from "./components/About";
-import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
-import Project from "./components/Project";
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Project from "./components/Project";
+import SinglePost from "./components/SinglePost";
 
 function App() {
   return (
     <BrowserRouter>
-    <NavBar />
+      <NavBar />
       <Switch>
-        <Route component={Home} path='/' exact />
-        <Route component={About} path='/about' />
-        <Route component={SinglePost} path='/post/:slug' />
-        <Route component={Post} path='/post' />
-        <Route component={Project} path='/project' />
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={SinglePost} path="/post:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
