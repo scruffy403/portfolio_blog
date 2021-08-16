d2d9424c7854b0634e1e5deb4fbf5b3705de778c
John <scruffy403@gmail.com>
2021-08-16 15:58:10 -0700

    Commit after deploying

--- src/components/About.js
+++ src/components/About.js
@@ -43,8 +43,7 @@ export default function About() {
           />
           <div className="text-lg flex flex-col justify-center">
             <h1 className="cursive text-6xl text-blue-300 mb-4">
-              Hey there! I'm{" "}
-              <span className="text-blue-100">{author.name}</span>
+              Hi! I'm <span className="text-blue-100">{author.name}</span>
             </h1>
             <div className="prose lg:prose-xl text-white">
               <BlockContent
