99bed3c287f1f0ff23c40256b9987742312046b0
John <scruffy403@gmail.com>
2021-08-31 14:39:19 +0100

    Changed style for smaller screens for Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,10 @@ export default function About() {
         alt="Closeup of guitar soundhole and strings"
         className="absolute object-cover w-full h-full"
       />
-      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-        <h1 className="text-4xl text-blue-400 font-bold cursive leading-none lg:leading-snug home-name">
+      <section className="relative flex justify-center min-h-screen pt-36 md:pt-48 lg:pt-64 px-8">
+        <h1 className="text:sm md:text-6xl lg:text-9xl text-blue-400 font-bold cursive leading-none lg:leading-snug home-name">
           Hello! I'm John David{" "}
-          <p className="text-4xl flex justify-center lg:leading-snug bg-black rounded-full hover:text-yellow-500">
+          <p className="text:sm md:text-xl flex justify-center lg:leading-snug bg-black rounded-full hover:text-yellow-500">
             <Link to="/nasaphoto">Have a look at the stars!</Link>
           </p>
         </h1>
