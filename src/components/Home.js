a6bff726d7b0861e09462ac6486e6975f32b751c
John Soto <scruffy403@gmail.com>
2022-04-01 13:02:59 +0100

    removed link to NASA Photo from landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,9 +13,6 @@ export default function About() {
       <section className="relative flex justify-center min-h-screen pt-36 md:pt-48 lg:pt-64 px-8">
         <h1 className="text:sm md:text-6xl lg:text-9xl text-blue-400 font-bold cursive leading-none lg:leading-snug home-name">
           Hello! I'm John David{" "}
-          <p className="text:sm md:text-xl flex justify-center lg:leading-snug bg-black rounded-full hover:text-yellow-500">
-            <Link to="/nasaphoto">Have a look at the stars!</Link>
-          </p>
         </h1>
       </section>
     </main>
